b2fef5417d878cebd8b3e2692cb79f1dbdb8ee2b
Tadeas Kmenta <kmentat@gmail.com>
2021-09-20 10:45:52 +0700

    adjust configs

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'max-len': [
+      'error',
+      {
+        code: 300,
+        ignoreUrls: true,
+        ignoreTrailingComments: true,
+      },
+    ],
+    'no-console': 'off',
+    'import/extensions': [
+      'error',
+      'never',
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
