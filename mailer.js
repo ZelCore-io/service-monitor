fcbcff3b3893e42e31fdba25088c7e6025d045d8
Ali Mahdavi <alihmahdavi@gmail.com>
2022-03-29 00:02:01 +0430

    added discord webhook to the mailer

--- mailer.js
+++ mailer.js
@@ -1,9 +1,29 @@
 const request = require('request-promise-native');
 const nodemailer = require('nodemailer');
+const webhook = require('@prince25/discord-webhook-sender');
 
 const url = 'https://status.zel.network';
 const user = 'zelservicemanager@gmail.com';
 const password = 'secretpassword';
+const web_hook_url = 'web_hook_url';
+
+function discord_hook(node_msg, web_hook_url, title, color, field_name) {
+  if (typeof web_hook_url !== 'undefined' && web_hook_url !== '0') {
+    
+    const Hook = new webhook.Webhook(`${web_hook_url}`);
+    Hook.setUsername('Service Monitor');
+    const msg = new webhook.MessageBuilder()
+      .setTitle(`:loudspeaker: **${title}**`)
+      .addField(`${field_name}:`, node_msg)
+      .setColor(`${color}`)
+    try {  
+      Hook.send(msg);
+      console.log('Sent to Discord');
+    } catch (error) {
+      console.log('Error Sending to Discord');
+    }
+  }
+}
 
 setInterval(() => {
   request({ uri: url, json: true })
@@ -26,7 +46,6 @@ setInterval(() => {
           subject: 'SERVICE down',
           text: JSON.stringify(response.errors),
         };
-
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
             console.log(error);
@@ -34,6 +53,7 @@ setInterval(() => {
             console.log(`Email sent: ${info.response}`);
           }
         });
+        discord_hook(JSON.stringify(response.errors), web_hook_url, 'SERVICE down', '#1F8B4C', 'Errors');
       } else {
         console.log('ALL OK');
       }
@@ -57,7 +77,7 @@ setInterval(() => {
         subject: 'MONITORING DOWN',
         text: 'Please, check monitoring server!',
       };
-
+      discord_hook('Please, check monitoring server!', web_hook_url, 'MONITORING DOWN', '#1F8B4C', 'Action');
       transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
           console.log(err);
@@ -66,4 +86,6 @@ setInterval(() => {
         }
       });
     });
-}, 6 * 60 * 1000);
+}, 6 * 60 * 10);
+
+
