388be828ff87dfbf0dff3d67cdcbc41f369b0125
TheTrunk <tadeas.kmenta@zel.cash>
2023-03-26 12:02:12 +0800

    rewrite to axios

--- mailer.js
+++ mailer.js
@@ -1,4 +1,4 @@
-const request = require('request-promise-native');
+const axios = require('axios');
 const nodemailer = require('nodemailer');
 
 const url = 'https://status.zel.network';
@@ -6,40 +6,8 @@ const user = 'zelcorebotmailer@gmail.com';
 const password = 'secretpassword';
 
 setInterval(() => {
-  request({ uri: url, json: true })
-    .then((response) => {
-      if (response.errors.length > 0) {
-        const transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true,
-          auth: {
-            user,
-            pass: password,
-          },
-        });
-
-        const mailOptions = {
-          from: user,
-          to: 'tadeas@zelcore.io',
-          cc: ['jeremy@runonflux.io'],
-          subject: 'SERVICE down',
-          text: JSON.stringify(response.errors),
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(`Email sent: ${info.response}`);
-          }
-        });
-      } else {
-        console.log('ALL OK');
-      }
-    })
-    .catch((error) => {
-      console.log(error);
+  axios.get(url).then((response) => {
+    if (response.data.errors.length > 0) {
       const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -53,17 +21,47 @@ setInterval(() => {
       const mailOptions = {
         from: user,
         to: 'tadeas@zelcore.io',
-        cc: ['jeremy@runonflux.io', 'greg@runonflux.io', 'gil@zelcore.io'],
-        subject: 'MONITORING DOWN',
-        text: 'Please, check monitoring server!',
+        cc: ['jeremy@runonflux.io'],
+        subject: 'SERVICE down',
+        text: JSON.stringify(response.data.errors),
       };
 
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          console.log(err);
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
         } else {
           console.log(`Email sent: ${info.response}`);
         }
       });
+    } else {
+      console.log('ALL OK');
+    }
+  }).catch((error) => {
+    console.log(error);
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user,
+        pass: password,
+      },
     });
+
+    const mailOptions = {
+      from: user,
+      to: 'tadeas@zelcore.io',
+      cc: ['jeremy@runonflux.io', 'greg@runonflux.io', 'gil@zelcore.io'],
+      subject: 'MONITORING DOWN',
+      text: 'Please, check monitoring server!',
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+  });
 }, 6 * 60 * 1000);
